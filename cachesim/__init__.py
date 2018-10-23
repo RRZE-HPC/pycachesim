455dc55c71f88fd3784cd90b22ebac949924650d
Julian Hammer <julian.hammer@fau.de>
2018-10-23 13:56:20 +0200

    version increment and switch to taging

--- cachesim/__init__.py
+++ cachesim/__init__.py
@@ -3,4 +3,11 @@ from __future__ import absolute_import
 
 from .cache import *
 
-__version__ = '0.1.7'
+__version__ = '0.1.8'
+
+# To trigger travis deployment to pypi, do the following:
+# 1. Increment __version___
+# 2. commit to RRZE-HPC/pycachesim's master branch
+# 3. wait for travis to complete successful (unless already tested)
+# 4. tag commit with 'v{}'.format(__version__) (`git tag vX.Y.Z`)
+# 5. push tag to github (`git push origin vX.Y.Z` or push all tags with `git push --tags`)
