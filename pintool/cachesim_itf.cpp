5d33c7a71f6266f5aa64cd12aa4204b368f7839e
Jo <johannes.wendler@fau.de>
2019-12-02 14:44:28 +0100

    functioning pintool to instrument between markers

--- pintool/cachesim_itf.cpp
+++ pintool/cachesim_itf.cpp
@@ -1,65 +1,162 @@
 #include "pin.H"
+#include "pinMarker.h"
 #include <iostream>
-#include <stdio.h>
-// #include <stddef.h>
-using std::cerr;
-using std::cout;
-using std::string;
-using std::endl;
+#include <fstream>
+#include <stdlib.h>
+
+KNOB<bool> KnobFollowCalls(KNOB_MODE_WRITEONCE, "pintool",
+    "follow_calls", "0", "specify if the instrumentation has to follow function calls between the markers");
+
+ADDRINT startCall;
+ADDRINT startIns;
+ADDRINT stopCall;
+ADDRINT stopIns;
+
+
+LOCALFUN VOID activate()
+{
+    _pinMarker_active = true;
+
+}
+LOCALFUN VOID deactivate()
+{
+    _pinMarker_active = false;
+}
+
+VOID ImageLoad(IMG img, VOID *v)
+{
+    if (IMG_IsMainExecutable(img))
+    {
+        for( SYM sym= IMG_RegsymHead(img); SYM_Valid(sym); sym = SYM_Next(sym) )
+        {
+            if (PIN_UndecorateSymbolName ( SYM_Name(sym), UNDECORATION_NAME_ONLY) == "_magic_pin_start")
+            {
+                startCall = SYM_Address(sym);
+            }
+            if (PIN_UndecorateSymbolName ( SYM_Name(sym), UNDECORATION_NAME_ONLY) == "_magic_pin_stop")
+            {
+                stopCall = SYM_Address(sym);
+            }
+        }
+
+        for( SEC sec= IMG_SecHead(img); SEC_Valid(sec); sec = SEC_Next(sec) )
+        {
+                for( RTN rtn= SEC_RtnHead(sec); RTN_Valid(rtn); rtn = RTN_Next(rtn) )
+                {
+                    RTN_Open(rtn);
+
+                    for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins))
+                    {
+                        if (INS_IsDirectControlFlow(ins))
+                        {
+                            if (INS_DirectControlFlowTargetAddress(ins) == startCall)
+                            {
+                                if (KnobFollowCalls)
+                                {
+                                    const AFUNPTR Activate = (AFUNPTR) activate;
+                                    INS_InsertPredicatedCall(ins, IPOINT_BEFORE, Activate, IARG_END);
+                                }
+                                else
+                                {
+                                    startIns = INS_Address(ins);
+                                }
+                            }
+                            if (INS_DirectControlFlowTargetAddress(ins) == stopCall)
+                            {
+                                if (KnobFollowCalls)
+                                {
+                                    const AFUNPTR Deactivate = (AFUNPTR) deactivate;
+                                    INS_InsertPredicatedCall(ins, IPOINT_BEFORE, Deactivate, IARG_END);
+                                }
+                                else
+                                {
+                                    stopIns = INS_Address(ins);
+                                }
+                            }
+                        }
+                    }
+                    RTN_Close(rtn);
+                }
+        }
+    }
+}
+
+LOCALFUN VOID MemRead_check(UINT64 addr, UINT32 size)
+{
+    if (_pinMarker_active)
+    std::cout << addr << " " << size << " " << 1 << "\n";
+}
 
 LOCALFUN VOID MemRead(UINT64 addr, UINT32 size)
 {
-    cout << addr << " " << size << " " << 1 << endl;
+    std::cout << addr << " " << size << " " << 1 << "\n";
+}
+
+LOCALFUN VOID MemWrite_check(UINT64 addr, UINT32 size)
+{
+    if (_pinMarker_active)
+    std::cout << addr << " " << size << " " << 0 << "\n";
 }
 
 LOCALFUN VOID MemWrite(UINT64 addr, UINT32 size)
 {
-    cout << addr << " " << size << " " << 0 << endl;
+    std::cout << addr << " " << size << " " << 0 << "\n";
 }
 
-VOID Instruction(INS ins, VOID *v){
-
-    const AFUNPTR readFun = (AFUNPTR) MemRead;
-    const AFUNPTR writeFun = (AFUNPTR) MemWrite;
-           
-    if (INS_IsMemoryRead(ins) && INS_IsStandardMemop(ins))
-    {
-
-        INS_InsertPredicatedCall(
-            ins, IPOINT_BEFORE, readFun,
-            IARG_MEMORYREAD_EA,
-            IARG_MEMORYREAD_SIZE,
-            IARG_END);
-    }
-    
-    if(INS_HasMemoryRead2(ins)){
-
-        INS_InsertPredicatedCall(
-            ins, IPOINT_BEFORE, readFun,
-            IARG_MEMORYREAD2_EA,
-            IARG_MEMORYREAD_SIZE,
-            IARG_END);
-
-    }
-
-    if(INS_IsMemoryWrite(ins)){
-
-        INS_InsertPredicatedCall(
-            ins, IPOINT_BEFORE, writeFun,
-            IARG_MEMORYWRITE_EA,
-            IARG_MEMORYWRITE_SIZE,
-            IARG_END);
-
-    }
-}
-
-VOID Fini(INT32 code, VOID *v)
+VOID Instruction(INS ins, VOID *v)
 {
-    // std::cout << "Finished";
+    if (KnobFollowCalls)
+    {
+        const AFUNPTR readFun = (AFUNPTR) MemRead_check;
+        const AFUNPTR writeFun = (AFUNPTR) MemWrite_check;
+        
+        if (INS_IsMemoryRead(ins) && INS_IsStandardMemop(ins))
+        {
+            INS_InsertPredicatedCall(
+                ins, IPOINT_BEFORE, readFun,
+                IARG_MEMORYREAD_EA,
+                IARG_MEMORYREAD_SIZE,
+                IARG_END);
+        }
+
+        if(INS_IsMemoryWrite(ins))
+        {
+            INS_InsertPredicatedCall(
+                ins, IPOINT_BEFORE, writeFun,
+                IARG_MEMORYWRITE_EA,
+                IARG_MEMORYWRITE_SIZE,
+                IARG_END);
+        }
+    }
+    else if(INS_Address(ins) > startIns && INS_Address(ins) < stopIns)
+    {
+        const AFUNPTR readFun = (AFUNPTR) MemRead;
+        const AFUNPTR writeFun = (AFUNPTR) MemWrite;
+        
+        if (INS_IsMemoryRead(ins) && INS_IsStandardMemop(ins))
+        {
+            INS_InsertPredicatedCall(
+                ins, IPOINT_BEFORE, readFun,
+                IARG_MEMORYREAD_EA,
+                IARG_MEMORYREAD_SIZE,
+                IARG_END);
+        }
+
+        if(INS_IsMemoryWrite(ins))
+        {
+            INS_InsertPredicatedCall(
+                ins, IPOINT_BEFORE, writeFun,
+                IARG_MEMORYWRITE_EA,
+                IARG_MEMORYWRITE_SIZE,
+                IARG_END);
+        }
+    }
 }
 
 int main(int argc, char *argv[])
 {
+
+    PIN_InitSymbols();
 
     // Initialize PIN library. Print help message if -h(elp) is specified
     // in the command line or the command line is invalid 
@@ -68,16 +165,19 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // add an instrumentation function
+    std::cerr << "starting" << std::endl;
+
+    if (KnobFollowCalls)
+    {
+        std::cerr << "follow calls" << std::endl;
+    }
+
+    IMG_AddInstrumentFunction(ImageLoad, 0);
+
     INS_AddInstrumentFunction(Instruction, 0);
-    
-    // Register function to be called when the application exits
-    PIN_AddFiniFunction(Fini, 0);
     
     // Start the program, never returns
     PIN_StartProgram();
     
     return 0;
 }
-
-
