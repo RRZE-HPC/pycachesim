fbbb2166b0bb1dc1961ae66c81bad46dec70fbc1
Julian Hammer <julian.hammer@fau.de>
2021-03-19 15:02:50 +0100

    fixed inclusion of backend.h file; version bump

--- cachesim/__init__.py
+++ cachesim/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 
 from .cache import *
 
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 
 # To trigger travis deployment to pypi, do the following:
 # 1. Increment __version___
