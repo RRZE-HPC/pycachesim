841fab53ad1ded7a52ad201d727672e39f03a5c6
Julian Hammer <julian.hammer@fau.de>
2021-04-15 16:33:50 +0200

    version bump

--- cachesim/__init__.py
+++ cachesim/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 
 from .cache import *
 
-__version__ = '0.2.5'
+__version__ = '0.3.0'
 
 # To trigger travis deployment to pypi, do the following:
 # 1. Increment __version___
